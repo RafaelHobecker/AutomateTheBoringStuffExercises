6451d4ecef83671bace5ba0f66161db0ebff37bf
Aldo Rafael Estigarribia Hobecker <aldorfaelhobecker04@gmail.com>
2025-12-13 11:04:42 -0300

    feat Cap4:Listas

--- Chapter 04/listas.py
+++ Chapter 04/listas.py
@@ -1,50 +1,50 @@
 
-spam = ['cat','bat','rat','elephant']
+animal = ['cat','bat','rat','elephant']
 
 #Negative Indexes
-print(spam[-1])
+print(animal[-1])
 
-print(spam[-3])
+print(animal[-3])
 
-print('The '+ spam[-1]+' is afraid of the ' + spam[-3]+'.')
+print('The '+ animal[-1]+' is afraid of the ' + animal[-3]+'.')
 
 # Getting Sublists with Slices
 
-print(spam[0:4])
+print(animal[0:4])
 
-print(spam[1:3])
+print(animal[1:3])
 
-print(spam[0:-1])
+print(animal[0:-1])
 
 # Leaving out the second index is the same as using the length of the list.
-print(spam[:2])
+print(animal[:2])
 
-print(spam[1:])
+print(animal[1:])
 
 #Leaving out the second index is the same as using the length of the list, which will slice to the end of the list.
-print(spam[:])
+print(animal[:])
 
 
 # Getting a Lists Length with len()
-print(f'La longitud de la lista es: {len(spam)}')
+print(f'La longitud de la lista es: {len(animal)}')
 
 #Changing Values in a List with Indexes
-spam[1]='chicken'
-print(spam)
+animal[1]='chicken'
+print(animal)
 
-spam[2]='monkey'
-print(spam)
+animal[2]='monkey'
+print(animal)
 
-spam[2]=spam[1]
-print(spam)
+animal[2]=animal[1]
+print(animal)
 
-spam[-1]=12345
-print(spam)
+animal[-1]=12345
+print(animal)
 
 # List Concatenation and List Replication
 
-new_spam = [1,2,3]+['A','B','C']
-print(new_spam)
+new_animal = [1,2,3]+['A','B','C']
+print(new_animal)
 
 triplica_lista = ['X','Y','Z']*3
 print(triplica_lista)
