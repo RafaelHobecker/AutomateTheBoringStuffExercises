35e75e8dc8946c295ccbe30776793ee794f70cee
Aldo Rafael Estigarribia Hobecker <aldorfaelhobecker04@gmail.com>
2026-01-28 14:23:54 -0300

    Cap06: Strings

---
+++ Chapter 06/strings1.py
@@ -0,0 +1,27 @@
+def sep():
+    print('\n')
+
+# Escape Characters
+
+
+spam = 'Say hi to Bob\'s mother.'
+print(spam)
+
+sep()
+
+spamFirst = 'Say hi to Bob\t mother'
+print(spamFirst)
+
+sep()
+
+spamSecond = 'Say hi to Bob\n mother'
+print(spamSecond)
+sep()
+
+
+spamThird = 'Say hi to Bob\\ mother'
+print(spamThird)
+
+sep()
+
+print("Hello there!\nHow are are you?\nI\'m doing fine.")
